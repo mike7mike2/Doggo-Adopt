6f0cd761977f52888a95dc3ec755d64f2805d62d
mike7mike2 <micah.wood27@icloud.com>
2020-01-29 12:11:24 -0600

    Fixed some formatting, the readme, and syncing up with local changes.

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,15 @@ function Header() {
         <span className="navLogo">DOGGO</span>
         <input type="text" placeholder="Search.." className="navSearch"></input>
         <input className="navButt" type="submit"></input>
-        <span className="navAdopt">ADOPT</span>
+        <div className="nav-adopt-dropdown">
+        <span>ADOPT</span>
+          <div className="adopt-dropdown-content">
+            <span>Adopt</span>
+            <span>Dogs</span>
+            <span>Cats</span>
+            <span>More</span>
+          </div>  
+        </div>
         <span className="navAbUs">ABOUT US</span>
       </div>
     </header>
